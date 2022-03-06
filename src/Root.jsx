fadfd92e496c11dc34b5c4a045c642d8a2c5e437
mrhrifat <mrhrifat383759@gmail.com>
2022-03-06 23:19:02 +0600

    remove: import of 3 component

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import Content from './components/Content'
-import ParticlesBG from './components/ParticlesBG'
-import Glow from './components/Glow'
+
 
 const Root = () => {
     return (
